9f9b3360a016fb90667172b71f8a96fcaf2a02e7
Oskar Wyke <oskar.wyke@gmail.com>
2016-01-10 19:12:38 +0100

    Fiddling with Iterable  vs Observable

--- src/main/java/se/mejsla/stepup/rxjava/Scratch.java
+++ src/main/java/se/mejsla/stepup/rxjava/Scratch.java
@@ -1,11 +1,40 @@
 package se.mejsla.stepup.rxjava;
 
 import rx.Observable;
+import rx.functions.Action0;
+import rx.functions.Action1;
+
+import java.util.Arrays;
 
 public class Scratch {
 
     public static void main(String[] args) {
-        Integer[] ints= {1,2,3,4,5};
-        Observable.from(ints).subscribe(System.out::println);
+        Iterable<Integer> ints= Arrays.asList(2,4,5,0);
+
+
+        Action1 errorHandler = (e) -> System.out.println("Cannot divide with zero, RTFM!");;
+        Action1<Integer> elementHandler = (i) -> System.out.println(100 / i);
+        Action0 finishedHandler = () -> System.out.println("Done!");
+
+        iterateEm(ints, elementHandler, errorHandler, finishedHandler);
+        observeEm(ints, elementHandler, errorHandler, finishedHandler);
+
+    }
+
+    private static void iterateEm(Iterable<Integer> ints, Action1<Integer> elementHandler, Action1 errorHandler, Action0 finishedHandler) {
+        try {
+            ints.forEach(elementHandler::call);
+        } catch (Exception ex) {
+            errorHandler.call(ex);
+        }
+        finishedHandler.call();
+
+
+    }
+
+    private static void observeEm(Iterable<Integer> ints, Action1<Integer> elementHandler, Action1 errorHandler, Action0 finishedHandler) {
+        Observable.from(ints)
+                .subscribe(elementHandler, errorHandler, finishedHandler);
+
     }
 }
