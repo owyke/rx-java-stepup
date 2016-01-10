573db8ee2bc3094654aed68ecf65c34c24ca3b53
Oskar Wyke <oskar.wyke@gmail.com>
2016-01-11 00:15:40 +0100

    Added another integer exersice

--- src/main/java/se/mejsla/stepup/rxjava/IntegerFiddler.java
+++ src/main/java/se/mejsla/stepup/rxjava/IntegerFiddler.java
@@ -7,6 +7,9 @@ public class IntegerFiddler {
 
 
 
+    public Observable<Integer> sumOfAllEvenNumbers(Observable<Integer> intObs) {
+        return intObs.filter((i) -> i % 2 == 0).reduce((i1,i2)-> i1 + i2);
+    }
 
     public Observable<Integer> sumPairs(Observable<Integer> intObs1, Observable<Integer> intObs2) {
         return intObs1.zipWith(intObs2, (i1, i2) -> i1 + i2);
--- src/test/java/se/mejsla/stepup/rxjava/IntegerFiddlerTest.java
+++ src/test/java/se/mejsla/stepup/rxjava/IntegerFiddlerTest.java
@@ -12,6 +12,16 @@ public class IntegerFiddlerTest {
 
     final IntegerFiddler instance = new IntegerFiddler();
 
+
+    @Test
+    public void testSumEqual() {
+        final TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
+        instance.sumOfAllEvenNumbers(Observable.just(2,7,13,4,8,44,5,7))
+            .subscribe(testSubscriber);
+        testSubscriber.assertReceivedOnNext(Arrays.asList(58));
+    }
+
+
     @Test
     public void testSumPairs() throws Exception {
         Observable result = instance.sumPairs(Observable.just(0,2,3,5), Observable.just(0,4,3,77));
