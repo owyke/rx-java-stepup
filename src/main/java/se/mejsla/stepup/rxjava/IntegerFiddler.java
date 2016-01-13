576f4017424c35fbdefa121fc5f9b1bc70dcf00a
Oskar Wyke <oskar.wyke@gmail.com>
2016-01-13 01:16:14 +0100

    WIP

--- src/main/java/se/mejsla/stepup/rxjava/IntegerFiddler.java
+++ src/main/java/se/mejsla/stepup/rxjava/IntegerFiddler.java
@@ -1,20 +1,32 @@
 package se.mejsla.stepup.rxjava;
 
+import javafx.util.Pair;
 import rx.Observable;
+import rx.Subscriber;
 
 
 public class IntegerFiddler {
 
 
 
-    public Observable<Integer> sumOfAllEvenNumbers(Observable<Integer> intObs) {
-        return intObs.filter((i) -> i % 2 == 0).reduce((i1,i2)-> i1 + i2);
+    public Observable sumOfAllEvenNumbers(Observable<Integer> o) {
+        return o.filter((i) -> i % 2 == 0).reduce((i1,i2)-> i1 + i2);
     }
 
-    public Observable<Integer> sumPairs(Observable<Integer> intObs1, Observable<Integer> intObs2) {
-        return intObs1.zipWith(intObs2, (i1, i2) -> i1 + i2);
+    public Observable sumPairs(Observable<Integer> o1, Observable<Integer> o2) {
+        return o1.zipWith(o2, (i1, i2) -> i1 + i2);
     }
 
 
+    public Observable sortIntegers(Observable<Integer> o1, Observable<Integer> o2) {
+        return o1.mergeWith(o2).toSortedList();
 
+
+    }
+
+    public Observable getEvens(Observable<Integer> o1, Observable<Integer> o2) {
+        return o1.mergeWith(o2).filter((i) -> i % 2 == 0);
+
+
+    }
 }
--- src/test/java/se/mejsla/stepup/rxjava/IntegerFiddlerTest.java
+++ src/test/java/se/mejsla/stepup/rxjava/IntegerFiddlerTest.java
@@ -5,13 +5,16 @@ import rx.Observable;
 import rx.observers.TestSubscriber;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
 public class IntegerFiddlerTest {
 
     final IntegerFiddler instance = new IntegerFiddler();
-
+    final Random rand = new Random(1337);
 
     @Test
     public void testSumEqual() {
@@ -29,5 +32,20 @@ public class IntegerFiddlerTest {
         result.subscribe(testSubscriber);
         testSubscriber.assertReceivedOnNext(Arrays.asList(0,6,6,82));
         testSubscriber.assertCompleted();
+    }
+
+
+    @Test
+    public void sortIntegers() throws Exception {
+        List<Integer> ints
+        Observable result = instance.sortIntegers(Observable.just(9,3,7,34,5,23), Observable.just(6,17,31,43,33,8));
+        TestSubscriber testSubscriber = new TestSubscriber();
+        result.subscribe(testSubscriber);
+        testSubscriber.assertReceivedOnNext(Arrays.asList(0,6,6,82));
+        testSubscriber.assertCompleted();
+    }
+
+    private List<Integer> createListOfIntegers(int len) {
+        return rand.ints(len).boxed().collect(Collectors.toList());
     }
 }
