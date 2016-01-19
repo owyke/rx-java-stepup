3ba0df03308b8cd912c9b82fbf62a610a320fcee
Oskar Wyke <oskar.wyke@gmail.com>
2016-01-20 00:29:57 +0100

    Polished the Observable contract laboration

--- src/main/java/se/mejsla/stepup/rxjava/contract/ObservableFactory.java
+++ src/main/java/se/mejsla/stepup/rxjava/contract/ObservableFactory.java
@@ -1,39 +1,47 @@
 package se.mejsla.stepup.rxjava.contract;
 
 import rx.Observable;
-import rx.Subscriber;
-
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.Collection;
 import java.util.function.Supplier;
 
 public class ObservableFactory {
 
-    public static <T> Observable<T> make(Supplier<T>... s) {
-        return make(Arrays.asList(s));
+    public static <T> Observable<T> fromSuppliers(Supplier<T>... s) {
+        return fromSuppliers(Arrays.asList(s));
     }
 
 
-    public static <T> Observable<T> make(final List<Supplier<T>> suppliers) {
-        return Observable.create(new Observable.OnSubscribe<T>() {
-            @Override
-            public void call(Subscriber<? super T> subscriber) {
-                for(Supplier<T> supplier : suppliers) {
-                    final T t;
-                    try {
-                        t = supplier.get();
-                    } catch (Throwable e) {
-                        subscriber.onError(new VerySpecialException(e));
-                        return;
-                    }
-                    subscriber.onNext(t);
-                };
-                subscriber.onCompleted();
+    /**
+     * This method takes an {@link Iterable<Supplier<T>>} and returns an {@link Observable<T>} that fulfils
+     * The Observable Contract. See: http://reactivex.io/documentation/contract.html
+     *
+     * @param suppliers - the sources of values for this Observable to emit to its subscribers.
+     * @param <T>       type of values emitted by the returned Observable
+     * @return
+     */
+    public static <T> Observable<T> fromSuppliers(final Collection<Supplier<T>>suppliers) {
+            return Observable.empty();
+            // TODO: Implement me. Maybe using Observable.create()....
 
-            }
-
-        });
+//        return Observable.create(new Observable.OnSubscribe<T>() {
+//            @Override
+//            public void call(Subscriber<? super T> subscriber) {
+//                for (Supplier<T> supplier : suppliers) {
+//                    final T t;
+//                    try {
+//                        t = supplier.get();
+//                    } catch (Throwable e) {
+//                        subscriber.onError(new VerySpecialException(e));
+//                        return;
+//                    }
+//                    subscriber.onNext(t);
+//                }
+//                subscriber.onCompleted();
+//
+//            }
+//
+//        });
     }
 }
